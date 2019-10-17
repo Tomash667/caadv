36c38ee283ef73788702a2afe5d653e6e9b0a445
Tomashu <tomash4my@gmail.com>
2019-10-17 19:36:24 +0200

    removed dx leaks, fullscreen

--- source/Game.cpp
+++ source/Game.cpp
@@ -25,4 +25,12 @@ void Game::OnUpdate(float dt)
 {
 	if(app::input->Shortcut(KEY_ALT, Key::F4) || app::input->Down(Key::Escape))
 		engine->Shutdown();
+	if(app::input->Shortcut(KEY_CONTROL, Key::U))
+		engine->UnlockCursor();
+	if(app::input->Shortcut(KEY_ALT, Key::Enter))
+		engine->SetFullscreen(!engine->IsFullscreen());
+	if(app::input->Pressed(Key::N1))
+		engine->SetWindowSize(Int2(1024, 768));
+	if(app::input->Pressed(Key::N2))
+		engine->SetWindowSize(Int2(1280, 1024));
 }
